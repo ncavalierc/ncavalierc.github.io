3d5335815083f3c38866890110796b80a8c4132a
ncavalierc <nicolas.cavalier-coulet@insa-lyon>
2022-03-31 21:44:35 +0200

    1.0

---
+++ js/popup.js
@@ -0,0 +1,7 @@
+function popupToggle(video){
+    const popup = document.querySelector('.wok')
+    popup.classList.toggle('hidden');
+}
+
+// <a onclick="popupToggle('wok')"><img src="img/wok.webp" alt="wok"></a>
+
---
+++ js/scripts.js
@@ -0,0 +1,133 @@
+/***************** Waypoints ******************/
+
+$(document).ready(function() {
+
+	$('.wp1').waypoint(function() {
+		$('.wp1').addClass('animated fadeInLeft');
+	}, {
+		offset: '75%'
+	});
+	$('.wp2').waypoint(function() {
+		$('.wp2').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp3').waypoint(function() {
+		$('.wp3').addClass('animated fadeInDown');
+	}, {
+		offset: '55%'
+	});
+	$('.wp4').waypoint(function() {
+		$('.wp4').addClass('animated fadeInDown');
+	}, {
+		offset: '75%'
+	});
+	$('.wp5').waypoint(function() {
+		$('.wp5').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp6').waypoint(function() {
+		$('.wp6').addClass('animated fadeInDown');
+	}, {
+		offset: '75%'
+	});
+
+
+	
+	var width = $(document).width();
+	var height = $(window).height();
+	var path = window.location.pathname;
+	var page = path.split("/").pop();
+
+
+	// contact center
+	if (page == 'contact.html'){
+		var contact = document.getElementById('contact');
+		let contactHeight = height - 121 - 74
+		contact.style.height = contactHeight.toString() + "px";
+	}
+	
+
+
+	// remove dot motim.pictures
+	if (page == 'index.html') {
+		if (width < 800) {
+			let titre = document.getElementById("titre")
+			titre.textContent="Pictures";
+		}
+	
+	}
+    
+
+});
+
+/***************** Slide-In Nav ******************/
+
+$(window).load(function() {
+
+	$('.nav_slide_button').click(function() {
+		$('.pull').slideToggle();
+	});
+
+});
+
+/***************** Smooth Scrolling ******************/
+
+$(function() {
+
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 2000);
+				return false;
+			}
+		}
+	});
+
+});
+
+
+/***************** Flexsliders ******************/
+
+$(window).load(function() {
+
+	$('#portfolioSlider').flexslider({
+		animation: "slide",
+		directionNav: false,
+		controlNav: true,
+		touch: false,
+		pauseOnHover: true,
+		start: function() {
+			$.waypoints('refresh');
+		}
+	});
+
+	$('#servicesSlider').flexslider({
+		animation: "slide",
+		directionNav: false,
+		controlNav: true,
+		touch: true,
+		pauseOnHover: true,
+		start: function() {
+			$.waypoints('refresh');
+		}
+	});
+
+	$('#teamSlider').flexslider({
+		animation: "slide",
+		directionNav: false,
+		controlNav: true,
+		touch: true,
+		pauseOnHover: true,
+		start: function() {
+			$.waypoints('refresh');
+		}
+	});
+
+});
