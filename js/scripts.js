62922c9841a0ad176b979c04d8ad4d2de7b36a04
ncavalierc <nicolas.cavalier-coulet@insa-lyon>
2023-03-19 14:54:03 +0100

    fix delay videos

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 /***************** Waypoints ******************/
 
 $(document).ready(function() {
@@ -136,3 +137,35 @@ $(window).load(function() {
 	});
 
 });
+
+
+$(function(){
+    let YouTubeContainers = document.querySelectorAll(".embed-youtube");
+
+    // Iterate over every YouTube container you may have
+    for (let i = 0; i < YouTubeContainers.length; i++) {
+        let container = YouTubeContainers[i];
+		let imageSource= "https://github.com/ncavalierc/images/raw/main/" + container.dataset.videoId.toString().toLowerCase().replace(/_/g, "-")  + ".webp";
+        // Load the Thumbnail Image asynchronously
+        let image = new Image();
+        image.src = imageSource;
+        image.addEventListener("load", function() {
+            container.appendChild(image);
+        });
+
+        // When the user clicks on the container, load the embedded YouTube video
+        container.addEventListener("click", function() {
+            let iframe = document.createElement( "iframe" );
+
+            iframe.setAttribute("frameborder", "0");
+            iframe.setAttribute("allowfullscreen", "");
+            iframe.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture");
+            // Important: add the autoplay GET parameter, otherwise the user would need to click over the YouTube video again to play it 
+            iframe.setAttribute("src", "https://www.youtube.com/embed/"+ this.dataset.videoId +"?rel=0&showinfo=0&autoplay=1");
+
+            // Clear Thumbnail and load the YouTube iframe
+            this.innerHTML = "";
+            this.appendChild( iframe );
+        });
+    }
+})();
